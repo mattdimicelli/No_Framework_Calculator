7c1270c71c3252960aa0ad9a1db48b1a887c4844
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-12-17 19:39:17 -0500

    format JS for readability

--- script.js
+++ script.js
@@ -34,10 +34,18 @@
                         handleOperator(btnValue);
                     })
                 }
-                else if (btnValue === '.') btn.addEventListener('click', handleDecimal);
-                else if (btnValue === '=') btn.addEventListener('click', handleEquals);
-                else if (btnValue === 'AC') btn.addEventListener('click', handleClear);
-                else if (btnValue === 'Backspace') btn.addEventListener('click', handleBackspace);
+                else if (btnValue === '.') {
+                    btn.addEventListener('click', handleDecimal);
+                } 
+                else if (btnValue === '=') {
+                    btn.addEventListener('click', handleEquals);
+                } 
+                else if (btnValue === 'AC') {
+                    btn.addEventListener('click', handleClear);
+                } 
+                else if (btnValue === 'Backspace') {
+                    btn.addEventListener('click', handleBackspace);
+                } 
 
                 function handleNumber(x) {
                     lastBtnPressWasNumberOrDecimalPoint = true;
@@ -48,7 +56,7 @@
                         populateDisplay(arg1);
                     }
                     else if (typeof operator === 'undefined') {
-                        modify(1, x);
+                        modifyArg(1, x);
                         populateDisplay(arg1);
                     }
                     else if (typeof arg2 === 'undefined') {
@@ -56,23 +64,30 @@
                         populateDisplay(arg2);
                     }
                     else {
-                        modify(2, x);         
+                        modifyArg(2, x);         
                         populateDisplay(arg2);                                              
                     }
                     
-                    console.log({arg1, arg2, operator, result, repeatWithSameArgAndOperator, repeatValue});
+                    console.log({
+                        arg1,
+                        arg2,
+                        operator,
+                        result,
+                        repeatWithSameArgAndOperator,
+                        repeatValue
+                    });
             
-                    function modify(operand, value) {
+                    function modifyArg(operand, value) {
                         if (operand === 1) {
-                            /* if arg1 is already 20 chars long, do nothing (to avoid storing
-                            numbers that are too long) */
+                            /* if arg1 is already 20 chars long, do nothing (to
+                            avoid storing numbers that are too long) */
                             if(arg1.length === 20) return;
                             else if(arg1 === '0') arg1 = value;
                             else arg1 += value; 
                         }
                         else if (operand === 2) {
-                            /* if arg2 is already 20 chars long, do nothing (to avoid storing
-                            numbers that are too long) */
+                            /* if arg2 is already 20 chars long, do nothing (to 
+                            avoid storing numbers that are too long) */
                             if(arg2.length === 20) return;
                             else if(arg2 === '0') arg2 = value;
                             else arg2 += value;
@@ -89,14 +104,11 @@
                         operator = newOp;
                     }
                     if (typeof arg1 === 'undefined') {
-                        // an operator can't do anything by itself, it needs an operand
+                        /* an operator can't do anything by itself, it needs an
+                        operand.... so do nothing */
                         return;
                     }
                     else if (typeof arg2 === 'undefined') {
-                        /* the operator in memory will be overwritten by newOp (if the user
-                        had previously input an operator), or it will be "initialized" by 
-                        newOp (if the user hadn't yet selected on of the operators for the
-                        equation) */
                         operator = newOp;
                     }
                     else {
@@ -108,7 +120,14 @@
                         arg2 = undefined;
                     }
             
-                    console.log({arg1, arg2, operator, result, repeatWithSameArgAndOperator, repeatValue});
+                    console.log({
+                        arg1,
+                        arg2,
+                        operator,
+                        result,
+                        repeatWithSameArgAndOperator,
+                        repeatValue
+                    });
                 }
             
                 function handleDecimal() {
@@ -119,9 +138,10 @@
                     }
                     else if (typeof operator === 'undefined') {
                         if (arg1.includes('.') || (arg1.length >= 19)) {
-                            /* if arg1 is already 19 chars long, there is no room for 
-                            a decimal point and another digit, so don't do anything.  Also,
-                            if arg1 already has a decimal point, it can't accept another */
+                            /* if arg1 is already 19 chars long, there is no 
+                            room for a decimal point and another digit, so don't
+                            do anything.  Also, if arg1 already has a decimal
+                            point, it can't accept another */
                             return;
                         }
                         arg1 += '.';
@@ -149,32 +169,36 @@
                         return;
                     }
                     else if (typeof operator === 'undefined') {
-                        /* continue to show the entered number on the screen, but delete arg1
-                        so that user can "start from the beginning" */
+                        /* continue to show the previously entered number on the
+                        screen, but delete arg1 so that user can "start fresh"
+                        by entering a new arg1 */
                         arg1 = undefined;
                         repeatWithSameArgAndOperator = false;
                         repeatValue = undefined;
                     }
                     else if (typeof arg2 === 'undefined') {
-                        /* if there is one number in the equation and the user has already 
-                        entered an operator, but then hits the equals button, perform the operation
-                        using the same number for arg2 as for arg1.  Ex: User hits "5", then "+",
-                        followed by "=".  The calculator will perform 5 + 5 and show the result.
-                        If the user presses equals again, the calculator will add 5 to the result 
-                        of the previous equation, and it will show 15.  User presses equals again,
-                        will show 20.  Etc, etc.  
-                        */
+                        /* if there is one number in the equation and the user
+                        has already entered an operator, but then hits the equals
+                        button, perform the operation using the same number for 
+                        arg2 as for arg1.  Ex: User hits "5", then "+", followed
+                        by "=".  The calculator will perform 5 + 5 and show the 
+                        result. If the user presses equals again, the calculator
+                        will add 5 to the result of the previous equation, and it 
+                        will show 15.  User presses equals again, will show 20. 
+                        Etc, etc.  */
                         if (!repeatWithSameArgAndOperator) {
                             repeatValue = arg1;
                             result = calculate();
                             populateDisplay(result);
                             arg1 = result;
                             repeatWithSameArgAndOperator = true;
-                            /* this flag will set the calculator to be in "repeat mode", which 
-                            performs the action described in the above comment every time the
-                            equals button is pressed, until the user breaks the pattern of repeatedly
-                            pressings equals, at which point the flag will be set to false, and
-                            the calculator will no longer be in "repeat mode" */
+                            /* this flag will set the calculator to be in "repeat
+                            mode", which performs the action described in the 
+                            above comment every time the equals button is 
+                            pressed, until the user breaks the pattern of 
+                            repeatedly pressings equals, at which point the flag
+                            will be set to false, and the calculator will no 
+                            longer be in "repeat mode" */
                         }
                         else if (repeatWithSameArgAndOperator) {
                             result = calculate();
@@ -191,7 +215,14 @@
                         repeatValue = false;
                         repeatValue = undefined;
                     }
-                    console.log({arg1, arg2, operator, result, repeatWithSameArgAndOperator, repeatValue});
+                    console.log({
+                        arg1,
+                        arg2,
+                        operator,
+                        result,
+                        repeatWithSameArgAndOperator,
+                        repeatValue
+                    });
                 }
             
                 function handleClear() {
@@ -261,7 +292,9 @@
                         correspondingBtn.classList.add('active');
                         correspondingBtn.click();
                     }
-                    else if (e.type === 'keyup') correspondingBtn.classList.remove('active');
+                    else if (e.type === 'keyup') {
+                        correspondingBtn.classList.remove('active');
+                    } 
                 }
             }
         }
@@ -275,32 +308,32 @@
         return result.toString();
 
         function operate(x, y, operator) {
-                switch (operator) {
-                    case '+':
-                        return add(x, y);
-                        break;
-                    case '-':
-                        return subtract(x, y);
-                        break;
-                    case '*':
-                        return multiply(x, y);
-                        break;
-                    case '/':
-                        return divide(x, y);
-                        break;
-                }
-                function add(x, y) {
-                    return x + y;
-                }
-                function subtract(x, y) {
-                    return x-y;
-                }
-                function multiply(x, y) {
-                    return x * y;
-                }
-                function divide(x, y) {
-                    return x / y;
-                }
+            switch (operator) {
+                case '+':
+                    return add(x, y);
+                    break;
+                case '-':
+                    return subtract(x, y);
+                    break;
+                case '*':
+                    return multiply(x, y);
+                    break;
+                case '/':
+                    return divide(x, y);
+                    break;
+            }
+            function add(x, y) {
+                return x + y;
+            }
+            function subtract(x, y) {
+                return x-y;
+            }
+            function multiply(x, y) {
+                return x * y;
+            }
+            function divide(x, y) {
+                return x / y;
+            }
         }
     }
 
@@ -312,17 +345,18 @@
             if (str.length > 20 && ((Number(str) > 1e19))) {
                 // if the number is huge, display it in scientific notation
                 str = Number(str).toExponential(14);
-                /* the scientific notation will show only 14 digits after the decimal
-                point, so that at the most, the result (string) will be a max of twenty
-                characters long, such as 9.999999999989e+24 */
+                /* the scientific notation will show only 14 digits after the 
+                decimal point, so that at the most, the result (string) will be 
+                a max of twenty characters long, such as 9.999999999989e+24 */
             }
             else if (str.length > 20) {
-                // else, if the string is too long b/c the number has too many decimal
-                // places, reduce the string to 20 chars max
+                /* else, if the string is too long b/c the number has too many 
+                decimal places, reduce the string to 20 chars max */
                 str = str.slice(0, 20);
-                // but if that means that the last char would be a decimal point, 
-                // cut off that decimal point as well
+                
                 if (str[19] === '.') str = str.slice(0,19);
+                /* if the last char would be a decimal point, cut off that 
+                decimal point as well */
             }
             return str;
         }
